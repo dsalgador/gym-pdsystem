51bcb5f1711284867c26440f5626950b08234539
dsalgador <daniel.salgado.rojo@gmail.com>
2018-04-09 12:19:08 +0200

    Add the max_episode_steps variable in the register call

--- gym_pdsystem/__init__.py
+++ gym_pdsystem/__init__.py
@@ -5,15 +5,11 @@ from gym.envs.registration import register
 logger = logging.getLogger(__name__)
 
 register(
-    id='PDSystem-v0',
-    entry_point='gym_pdsystem.envs:PDSystemEnv'#,
+    id='PDSystemEnv-v0',
+    entry_point='gym_pdsystem.envs:PDSystemEnv',
+    max_episode_steps=200,
+#,
     #timestep_limit=1000,
     #reward_threshold=1.0,
     #nondeterministic = True,
-)
-
-register(
-    id='Pendulum-v0',
-    entry_point='gym.envs.classic_control:PendulumEnv',
-    max_episode_steps=200,
 )
