00ca888397b6d45a3df6770c12d49fb5c8806eb0
dsalgador <daniel.salgado.rojo@gmail.com>
2018-04-17 11:10:04 +0200

    Deleted render2 that was for pendulum

--- gym_pdsystem/envs/pdsystem_env.py
+++ gym_pdsystem/envs/pdsystem_env.py
@@ -287,51 +287,8 @@ class PDSystemEnv(gym.Env):
         #self.tanktrans.set_translation(0, 0)
         self.tanktrans.set_scale(1,y/100)
 
-
-    
-          
-
         return self.viewer.render(return_rgb_array = mode=='rgb_array')
 
-        # if self.viewer is None:
-        #     import numpy as np
-        #     import matplotlib.pyplot as plt
-        # plt.axis([0, 3, 0, 1.2])
-
-
-        # index = np.array([i for i in range(3)]) + 0.5
-        # plt.bar(index, 1, color = "Black")
-        # colors = np.random.uniform(0,1,size = 3)
-        # plt.bar(index, colors, color = "blue")
-        
-        
-        # return(plt.show())
-
-    def render2(self, mode='human'):
-
-        if self.viewer is None:
-            from gym.envs.classic_control import rendering
-            self.viewer = rendering.Viewer(500,500)
-            self.viewer.set_bounds(-2.2,2.2,-2.2,2.2)
-            rod = rendering.make_capsule(1, .2)
-            rod.set_color(.8, .3, .3)
-            self.pole_transform = rendering.Transform()
-            rod.add_attr(self.pole_transform)
-            self.viewer.add_geom(rod)
-            axle = rendering.make_circle(.05)
-            axle.set_color(0,0,0)
-            self.viewer.add_geom(axle)
-            #fname = path.join(path.dirname(__file__), "assets/clockwise.png")
-            #self.img = rendering.Image(fname, 1., 1.)
-           # self.imgtrans = rendering.Transform()
-            #self.img.add_attr(self.imgtrans)
-
-        #self.viewer.add_onetime(self.img)
-        self.pole_transform.set_rotation(self.state[0] + np.pi/2)
-        #if self.last_u:
-         #   self.imgtrans.scale = (-self.last_u/2, np.abs(self.last_u)/2)
-
-        return self.viewer.render(return_rgb_array = mode=='rgb_array')
 
     def close(self):
         if self.viewer: self.viewer.close()
